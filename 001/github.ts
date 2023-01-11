760fa429f6b9e1f4d1073f46ef5083503935cf7a
json muchiri <jysonmuchiri@gmail.com>
2023-01-11 12:54:47 -0800

    Initial project commit

--- 001/github.ts
+++
@@ -1,7 +0,0 @@
-const user:string = "John Doe";
-
-console.log(user);
-
-let user2 = 'jane doe';
-
-user2 = 'nancy one'
--- 001/myFunc.js
+++
@@ -1,5 +0,0 @@
-function sayHello(message) {
-    return message;
-}
-var mike = sayHello("Hello Mike");
-console.log(mike);
